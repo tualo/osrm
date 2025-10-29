d145bbd2a303037a20523a6a893c10fcf94505f3
Thomas Hoffmann <t.hoffmann.1979@googlemail.com>
2025-10-29 22:34:29 +0100

    Version bump to 1.0.2

--- src/Routes/OsrmRoute.php
+++ src/Routes/OsrmRoute.php
@@ -1,22 +1,25 @@
 <?php
+
 namespace Tualo\Office\OSRM\Routes;
 
 use Tualo\Office\Basic\TualoApplication as App;
 use Tualo\Office\Basic\Route as BasicRoute;
 use Tualo\Office\Basic\IRoute;
 
-class OsrmRoute implements IRoute{
-    public static function register(){
-        BasicRoute::add('/osrm/route',function($matches){
-            try{
+class OsrmRoute extends \Tualo\Office\Basic\RouteWrapper
+{
+    public static function register()
+    {
+        BasicRoute::add('/osrm/route', function ($matches) {
+            try {
                 $db = App::get('session')->getDB();
                 App::contenttype('application/json');
-                App::set('hlsJobDir',HLS_JOB_DIR);
-                
-                App::result('success',true);
-            }catch(\Exception $e){
+                App::set('hlsJobDir', HLS_JOB_DIR);
+
+                App::result('success', true);
+            } catch (\Exception $e) {
                 App::result('msg', $e->getMessage());
             }
-        },array('get','post'),true);
+        }, array('get', 'post'), true);
     }
 }
